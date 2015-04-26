65fa423381f1fcb867acbe1a3bc58e3e9ea4195c
Vikrant Varma <vikrant.varma94@gmail.com>
2015-04-26 10:51:29 +0530

    Demo 1

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 var server = require('./lib/server');
+var log = require('./lib/log');
+var app = require('http').createServer();
+var io = require('socket.io')(app);
+
+app.listen(9000);
+
+var connections = {};
+
+io.on('connection', function(socket) {
+    log.info('Got connection new connection');
+    socket.on('registration', function(data) {
+        log.info({agentName: data.name}, 'New agent registered');
+        connections[data.name] = socket;
+    });
+    socket.on('disconnect', function() {
+        for (var key in connections) {
+            if (connections.hasOwnProperty(key)) {
+                if (connections[key] === this) {
+                    log.warn({agentName: key}, 'Agent disconnected');
+                }
+            }
+        }
+        io.emit('agent disconnected');
+    });
+});
 
 server.start();
